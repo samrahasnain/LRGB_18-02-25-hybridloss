267a88b1da44da056866d41fa802c00ff337b3e7
samrahasnain <143751042+samrahasnain@users.noreply.github.com>
2025-04-27 15:55:22 +0500

    Update solver.py

--- solver.py
+++ solver.py
@@ -520,7 +520,7 @@ class Solver(object):
                
                 start_time = time.time()  # Timing starts AFTER synchronization
                 #preds, _, _, _, _, _ = self.scripted_net(images)
-                preds, _, _ = self.scripted_net(images)
+                preds, _, _,_,_ = self.scripted_net(images)
                 if self.config.cuda:
                     torch.cuda.synchronize()
 
